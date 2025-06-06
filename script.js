473999c74aefba0dfc6c2aebe05b4b3dfb0221ce
Sepidehrfi <sepidehrafiee.k.com>
2025-06-06 13:20:03 +0330

    feat: added more cards and change count card game

--- script.js
+++ script.js
@@ -1,65 +1,96 @@
-const cards = document.querySelectorAll(".card");
+const cardsContainer = document.querySelector(".cards");
+const startBtn = document.getElementById("startGame");
+const input = document.getElementById("pairCount");
 
+let cards = [];
 let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
+let totalPairs = 8;
+
+
+function createCards() {
+  cardsContainer.innerHTML = "";
+  matched = 0;
+  cardOne = cardTwo = null;
+  disableDeck = false;
+
+  const cardValues = Array.from({ length: totalPairs }, (_, i) => i + 1);
+  const shuffled = [...cardValues, ...cardValues].sort(() => Math.random() - 0.5);
+
+  shuffled.forEach((val) => {
+    const card = document.createElement("li");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <div class="view front-view">
+        <img src="images/que_icon.svg" alt="icon">
+      </div>
+      <div class="view back-view">
+        <img src="images/img-${val}.png" alt="card-img">
+      </div>
+    `;
+
+    card.addEventListener("click", flipCard);
+    cardsContainer.appendChild(card);
+  });
+
+  cards = document.querySelectorAll(".card");
+}
+
 
 function flipCard({ target: clickedCard }) {
-  if (cardOne !== clickedCard && !disableDeck) {
+  if (cardOne !== clickedCard && !disableDeck && clickedCard.classList.contains("card")) {
     clickedCard.classList.add("flip");
+
     if (!cardOne) {
       return (cardOne = clickedCard);
     }
+
     cardTwo = clickedCard;
     disableDeck = true;
-    let cardOneImg = cardOne.querySelector(".back-view img").src,
-      cardTwoImg = cardTwo.querySelector(".back-view img").src;
-    matchCards(cardOneImg, cardTwoImg);
+
+    const img1 = cardOne.querySelector(".back-view img").src;
+    const img2 = cardTwo.querySelector(".back-view img").src;
+
+    matchCards(img1, img2);
   }
 }
 
 function matchCards(img1, img2) {
   if (img1 === img2) {
     matched++;
-    if (matched == 8) {
-      setTimeout(() => {
-        return shuffleCard();
-      }, 1000);
+    if (matched === totalPairs) {
+      setTimeout(() => createCards(), 1000);
     }
     cardOne.removeEventListener("click", flipCard);
     cardTwo.removeEventListener("click", flipCard);
-    cardOne = cardTwo = "";
-    return (disableDeck = false);
-  }
-  setTimeout(() => {
-    cardOne.classList.add("shake");
-    cardTwo.classList.add("shake");
-  }, 400);
-
-  setTimeout(() => {
-    cardOne.classList.remove("shake", "flip");
-    cardTwo.classList.remove("shake", "flip");
-    cardOne = cardTwo = "";
+    cardOne = cardTwo = null;
     disableDeck = false;
-  }, 1200);
+  } else {
+    setTimeout(() => {
+      cardOne.classList.add("shake");
+      cardTwo.classList.add("shake");
+    }, 400);
+    setTimeout(() => {
+      cardOne.classList.remove("shake", "flip");
+      cardTwo.classList.remove("shake", "flip");
+      cardOne = cardTwo = null;
+      disableDeck = false;
+    }, 1200);
+  }
 }
 
-function shuffleCard() {
-  matched = 0;
-  disableDeck = false;
-  cardOne = cardTwo = "";
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-  arr.sort(() => (Math.random() > 0.5 ? 1 : -1));
-  cards.forEach((card, i) => {
-    card.classList.remove("flip");
-    let imgTag = card.querySelector(".back-view img");
-    imgTag.src = `images/img-${arr[i]}.png`;
-    card.addEventListener("click", flipCard);
-  });
-}
 
-shuffleCard();
-
-cards.forEach((card) => {
-  card.addEventListener("click", flipCard);
+startBtn.addEventListener("click", () => {
+  const val = parseInt(input.value);
+  if (val >= 2 && val <= 16) {
+    totalPairs = val;
+    createCards();
+  } else {
+    alert("The number must be between 2 and 16.");
+  }
 });
+
+
+createCards();
